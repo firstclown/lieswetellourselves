fe4b045b2fb88932ccb3b9fdfedf83c9da936795
Joe Erickson <firstclown@firstclown.us>
2008-08-28 09:18:54 -0400

    Adding in django serving of files for the dev server, but not prod

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +18,5 @@ urlpatterns = patterns('',
     (r'', include('lieswetellourselves.lies.urls')),
 
 )
+if settings.DEBUG:
+        urlpatterns += patterns('', (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),) 
